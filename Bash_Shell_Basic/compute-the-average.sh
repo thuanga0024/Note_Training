d74d3f1b386fb6e86ad13b17581c481d05c9aec6
thuanga0024 <thuan.pro.7792@gmail.com>
2022-01-26 11:33:08 +0700

    Training_HackerRank

--- Bash_Shell_Basic/compute-the-average.sh
+++ Bash_Shell_Basic/compute-the-average.sh
@@ -3,16 +3,12 @@
 
 read -p "Nhap Vao So Luong Con So Can Tinh: " abc
 
-values=0
-
 for ((i=1; i<=$abc; i++))
 do
 	read arr_number[$i]
-	let " values+= ${arr_number[$i]}"
+	let " values+= ${arr_number[$i]} "
 done
 
-echo $values
-echo $abc
 echo "scale=3; $values/$abc" | bc
 
 
